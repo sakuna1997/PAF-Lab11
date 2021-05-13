c8892125a74115e74ff6776177354edf5297f27f
sakuna97 <subi222222@gmail.com>
2021-05-14 03:01:17 +0530

    1.3

--- Lab11/src/com/Item.java
+++ Lab11/src/com/Item.java
@@ -194,5 +194,27 @@ public class Item {
 		 } 
 		 return output; 
 		 } 
+	
+	public String login(String un, String pw)
+	{ 
+		String output = ""; 
+		
+		if (!un.equals("asd"))//Hard coded test value 
+		{ 
+			output = "Invalid username"; 
+		} 
+		else if (un.equals("asd"))//Hard coded test value 
+		{ 
+			if (!pw.equals("asd"))//Hard coded test value 
+			{ 
+				output = "Invalid password"; 
+			} 
+			else if (pw.equals("asd"))//Hard coded test value 
+			{ 
+				output = "success"; 
+			} 
+		} 
+		return output; 
+	}
 
 }
